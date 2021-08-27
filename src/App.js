c279aa25496606d0c58f0817e05c56781cca9b79
Euhid Aman <aman.derax20@gmail.com>
2021-08-27 21:17:00 +0530

    Added package-lock.json to gitignore

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center text-red-800">Hello World</h1>
+      <img src="https://source.unsplash.com/random/300×300/?sharks" className="w-32 h-32" alt="logo" />
     </div>
   );
 }
