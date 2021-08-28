ff3c909660ca1bda631d95a0dc6f7f57946fa28b
Euhid Aman <aman.derax20@gmail.com>
2021-08-28 17:24:03 +0530

    Added dummy navbar with blahaj logo from tailwind components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-center text-red-800">Hello World</h1>
-      <img src="https://source.unsplash.com/random/300×300/?sharks" className="w-32 h-32" alt="logo" />
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
